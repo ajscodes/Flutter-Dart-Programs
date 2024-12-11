a1a580dbd4d9aab5559c4936e13b168719e09d6e
ajscodes <ayushmaradia@gmail.com>
2024-12-11 21:51:27 +0530

    .

--- Lab1/A3.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main(){
-  print("Enter Fahrenheit: ");
-  double fehrenheit = double.parse(stdin.readLineSync()!);
-
-  double formula = (fehrenheit - 32)*(5/9);
-  print(formula);
-}
