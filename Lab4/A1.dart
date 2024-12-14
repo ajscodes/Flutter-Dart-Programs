0845ab69c14bae805ebbc8fb0ca18e7dab18406b
ajscodes <ayushmaradia@gmail.com>
2024-12-14 22:43:59 +0530

    Lab-4

---
+++ Lab4/A1.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main(){
+  stdout.write("Enter Principle: ");
+  double principle = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter Rate: ");
+  double rate = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter Time(option): ");
+  String? timeInput = stdin.readLineSync();
+  double time = (timeInput != null && timeInput.isNotEmpty) ? double.parse(timeInput) : 1;
+
+
+  double interest = simpleInterest(principle: principle, rate: rate,time: time);
+  print("Simple interest: $interest");
+}
+
+double simpleInterest(
+{required double principle, required double rate, double time = 1}){
+  return (principle * rate * time) / 100;
+}
